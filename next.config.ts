59094b928c5af8a6ca4caa794dd411945dd121e7
Inam <mfihassan99@gmail.com>
2025-12-25 10:15:42 +0530

    initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
+import { dirname } from "node:path";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+  turbopack: {
+    root: dirname(__filename),
+  },
 };
 
 export default nextConfig;
