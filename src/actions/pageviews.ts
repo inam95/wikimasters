4cf461bfb4ff66c1ea59bbb6fb62e45174104af0
Inam <mfihassan99@gmail.com>
2025-12-26 18:03:25 +0530

    react email integration

--- src/actions/pageviews.ts
+++ src/actions/pageviews.ts
@@ -1,11 +1,19 @@
 "use server";
 
 import redis from "@/cache";
+import sendCelebrationEmail from "@/email/celebration-email";
+
+const mileStones = [10, 50, 100, 1000, 10000];
 
 const keyFor = (id: number) => `pageviews:article:${id}`;
 
 export async function incrementPageView(id: number) {
   const articleKey = keyFor(id);
   const newVal = await redis.incr(articleKey);
+
+  if (mileStones.includes(newVal)) {
+    sendCelebrationEmail(id, newVal);
+  }
+
   return +newVal;
 }
---
+++ src/email/celebration-email.tsx
@@ -0,0 +1,57 @@
+import { render } from "@react-email/render";
+import { eq } from "drizzle-orm";
+import db from "@/db";
+import { articles, usersSync } from "@/db/schema";
+import resend from "@/email";
+import CelebrationTemplate from "./templates/celebration-template";
+
+const baseUrl = process.env.VERCEL_URL
+  ? `https://${process.env.VERCEL_URL}`
+  : `http://localhost:3000`;
+
+export default async function sendCelebrationEmail(
+  articleId: number,
+  pageviews: number,
+) {
+  const response = await db
+    .select({
+      email: usersSync.email,
+      id: usersSync.id,
+      name: usersSync.name,
+      articleTitle: articles.title,
+    })
+    .from(articles)
+    .leftJoin(usersSync, eq(articles.authorId, usersSync.id))
+    .where(eq(articles.id, articleId));
+
+  const { email, id, name, articleTitle } = response[0];
+
+  if (!email) {
+    return;
+  }
+
+  const emailRes = await resend.emails.send({
+    from: "Wikimasters <onboarding@resend.dev>",
+    to: "mfihassan99@gmail.com",
+    subject: `✨ Your article on Wikimasters has reached ${pageviews} pageviews!`,
+    react: (
+      <CelebrationTemplate
+        name={name ?? "User"}
+        pageviews={pageviews}
+        articleTitle={articleTitle}
+        articleUrl={`${baseUrl}/articles/${articleId}`}
+      />
+    ),
+  });
+
+  if (!emailRes.error) {
+    console.log(
+      `📧 sent ${id} a celebration for getting ${pageviews} on article ${articleId}`,
+    );
+  } else {
+    console.log(
+      `❌ error sending ${id} a celebration for getting ${pageviews} on article ${articleId}`,
+      emailRes.error,
+    );
+  }
+}
